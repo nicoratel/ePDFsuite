e95673ab6f6ee85ec9d19629677c31229c2506e0
Nicolas Ratel-Ramond <145773633+nicoratel@users.noreply.github.com>
2026-02-20 07:27:35 +0100

    Refactor imports and update version number
    Updated version to 0.1.1 and modified imports.

--- __init__.py
+++ __init__.py
@@ -1,14 +1,18 @@
 """ePDFsuite package"""
 
 # Imports absolus pour charger les modules
-from ePDFsuite.ePDFsuite import SAEDProcessor
-from ePDFsuite.filereader import load_data
+from .ePDFsuite import SAEDProcessor
+from .ePDFsuite import extract_ePDF_from_mutliple_files
+from .filereader import load_data
+# from .pdfanalysis import perform_automatic_pdf_analysis  # Module not found
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 # Ce que les utilisateurs peuvent importer
 __all__ = [
     'SAEDProcessor',
     'load_data',
+	'extract_ePDF_from_multiple_files',
+	'perform_automatic_pdf_analysis',
     
 ]
